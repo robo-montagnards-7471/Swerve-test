fc1e6ae6a860bc11721c278a4520d307f35bcb96
Vincent <vincenttueminh@gmail.com>
2025-11-26 19:23:57 -0500

    (à tester) code pour tester la valeur optimale pour la division de la vitesse

--- src/main/java/frc/robot/components/Motor.java
+++ src/main/java/frc/robot/components/Motor.java
@@ -17,40 +17,59 @@ public class Motor {
     private double objective_position = 0;
 
     private double speed = 0;
+    private double divider = 20;
+
+    private double optimal_divider = 20;
+    private double optimal_precision = 1;
+
+    private double last_position = 0;
 
     public Motor()
     {
         motor = new SparkMax(8, SparkLowLevel.MotorType.kBrushless);
         encoder = motor.getEncoder();
+        last_position = encoder.getPosition();
     }
 
     public void updateData() {
         double current_position = encoder.getPosition();
-        speed = Math.abs((objective_position-current_position)/20);
+        speed = Math.abs((objective_position-current_position)/divider);
         SmartDashboard.putNumber("Encoder Position", encoder.getPosition());
         SmartDashboard.putNumber("Encoder Velocity", encoder.getVelocity());
         SmartDashboard.putNumber("Objective Position", objective_position);
         SmartDashboard.putNumber("Speed", speed);
+        SmartDashboard.putNumber("Divider", divider);
+        SmartDashboard.putNumber("Optimal Divider", optimal_divider);
+        SmartDashboard.putNumber("Optimal Precision", optimal_precision);
+        SmartDashboard.putNumber("Distance To Go", Math.abs(current_position - objective_position));
     }
 
     public void poll() {
         double current_position = encoder.getPosition();
         if( current_position < objective_position ) {
-            setMotor(speed);
+            motor.set(speed);
         }
         else if( current_position > objective_position ) {
-            setMotor(speed*-1);
+            motor.set(speed*-1);
         }
         else {
-            setMotor(0);
+            motor.set(0);
         }
-    }
-
-    public void setMotor( double speed ) {
-        motor.set(speed);
+        if( current_position == last_position ) {
+            double current_precision = Math.abs(objective_position - current_position);
+            if( current_precision < optimal_precision ) {
+                divider = optimal_divider;
+                optimal_precision = current_precision;
+            }
+        }
+        last_position = current_position;
     }
 
     public void goToPosition( double position ) {
         objective_position = position;
+    }
+
+    public void incrementDivider() {
+        divider++;
     }
 }
