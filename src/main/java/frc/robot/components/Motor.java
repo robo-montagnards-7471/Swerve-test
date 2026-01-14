59721dbde9f84bff3e121ddff8fdbafb7df3ce47
Vincent <vincenttueminh@gmail.com>
2026-01-14 18:20:45 -0500

    everything displays in reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeed

--- src/main/java/frc/robot/components/Motor.java
+++ src/main/java/frc/robot/components/Motor.java
@@ -17,6 +17,7 @@ import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.math.controller.PIDController;
+import com.revrobotics.CANSparkMax;
 
 
 
